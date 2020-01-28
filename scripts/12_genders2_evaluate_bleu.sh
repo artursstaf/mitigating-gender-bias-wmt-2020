5d8fd9fe89aeac85f6d126a9fc9e36ed6eb8dfb3
Artūrs Stafanovičs <arturs.stafanovics@gmail.com>
2020-01-28 19:39:36 +0200

    naming fix + add results

--- scripts/12_genders2_evaluate_bleu.sh
+++ scripts/12_genders2_evaluate_bleu.sh
@@ -7,7 +7,7 @@ mkdir -p evaluation_logs/genders2
 for type in "true " "unkown u-" "random r-"; do
     set -- $type
 
-    python -m sockeye.translate -m models/nmt_genders2 --input-factors data/$2newsdev2017.genders.BPE.en --input data/newsdev2017.tc.BPE.en |
+    python -m sockeye.translate -m models/nmt_genders2 --input-factors data/newsdev2017.$2genders.BPE.en --input data/newsdev2017.tc.BPE.en |
         sed -r 's/@@( |$)//g' >data/dev_translations/genders2/$1_newsdev2017.tc.lv
 
     python -m sockeye.evaluate \
